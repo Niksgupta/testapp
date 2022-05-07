031003e97307612b27d22033cf12678680dc643c
nikhil <nikhilatoliya12@gmail.com>
2022-05-07 23:14:18 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <center>
+          <h1>This is Your First React Test Application</h1>
+        </center>
+      </div>
+    </>
+  );
+}
+
+export default App;
